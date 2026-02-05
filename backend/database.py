b1816226b8da465aac8502a3b5125feca0d1e581
Mohit2705-1982 <mohitjspr@gmail.com>
2026-02-05 12:21:19 +0530

    switch to postgres

--- backend/database.py
+++ backend/database.py
@@ -4,9 +4,5 @@ from sqlalchemy.orm import sessionmaker
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(
-    DATABASE_URL,
-    connect_args={"ssl": {"ssl_mode": "REQUIRED"}}
-)
-
+engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine)
