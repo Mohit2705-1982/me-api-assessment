086740efc2e30559e14401063e399eacea9e8fe5
Mohit2705-1982 <mohitjspr@gmail.com>
2026-02-05 12:13:33 +0530

    railway ssl fix

--- backend/database.py
+++ backend/database.py
@@ -4,5 +4,9 @@ from sqlalchemy.orm import sessionmaker
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={"ssl": {"ssl_mode": "REQUIRED"}}
+)
+
 SessionLocal = sessionmaker(bind=engine)
